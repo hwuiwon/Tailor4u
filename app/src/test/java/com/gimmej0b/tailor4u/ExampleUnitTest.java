c9fb184556ce1b18b25d9fc6b6ad4dbf1e722b63
Kevin Park <kevin.park1217@gmail.com>
2018-10-28 03:43:19 -0400

    Hello, World

---
+++ app/src/test/java/com/gimmej0b/tailor4u/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.gimmej0b.tailor4u;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
