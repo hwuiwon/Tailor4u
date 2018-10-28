632bbf6c496de59d8e458a971fecf24531234807
Kevin Park <kevin.park1217@gmail.com>
2018-10-28 05:07:33 -0400

    AR demo

--- app/src/main/java/com/gimmej0b/tailor4u/MainActivity.java
+++ app/src/main/java/com/gimmej0b/tailor4u/MainActivity.java
@@ -6,17 +6,24 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     private ArFragment arFragment;
-    private ModelRenderable andyRenderable;
+    private ModelRenderable tessaRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.tessa)
+                .build()
+                .thenAccept(renderable -> tessaRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load tessa renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (tessaRenderable == null) {
+                        return;
+                    }
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    // Create the transformable tessa and add it to the anchor.
+                    TransformableNode tessa = new TransformableNode(arFragment.getTransformationSystem());
+                    tessa.setParent(anchorNode);
+                    tessa.setRenderable(tessaRenderable);
+                    tessa.select();
+                });
+
     }
 
 
